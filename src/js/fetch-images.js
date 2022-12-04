dde98a76219fea4a652be128e58f2cdf0f54033f
Ihor Smyk <ismyk256@gmail.com>
2022-12-04 14:33:19 +0200

    upd

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export { fetchImages };
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-const KEY = '25766392-01b12b6ed5ab34bc2910d9c3e';
+const KEY = '31809187-ae0bedc556567e073ef43254d';
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
